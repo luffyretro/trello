936b9cc3354d1339e0568bc16dc5f4cbae6bbd95
Nmampuru <nmampuru@retrorabbit.co.za>
2019-07-11 21:41:53 +0200

    Title navbar up

---
+++ src/components/Nav/BoarderHeaderNav.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function BoarderHeaderNav(){
+    return(
+        <div>
+            <div className="board-menu">
+                <div className="board-menu--header">
+                    <h2 className="board-menu--title">{title}</h2>
+                    <button className="board-menu--x" onClick={handleShowMenu}><i className="fas fa-times"></i></button>
+                </div>
+
+                <div className="board-menu--form">
+                    <form onSubmit={handleNameSubmit}>
+                    <label>{inputLabel}</label>
+                    <input
+                        className="board-menu--input"
+                        type="text"
+                        name="name"
+                        placeholder={placeholder}
+                    />
+                    <button className="menu-btn button-green">{buttonLabel}</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BoarderHeaderNav
---
+++ src/components/Nav/TitleNavBar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function TitleNavBar (){
+    return(
+        <div className="top-nav">
+            <span className="home-btn"><i class="fas fa-home"></i></span>
+            <h1 className="title-nav">Trello</h1>
+        </div>
+    )
+}
+
+export default TitleNavBar
